c9fdf432384ddb1206abc5779bb3ca7b3b2aea2e
Enid Rapp <erapp901@gmail.com>
2026-07-04 19:22:38 -0700

    added db write function decl

--- sqldatabase.h
+++ sqldatabase.h
@@ -1,10 +1,13 @@
 #ifndef SQLDATABASE_H
 #define SQLDATABASE_H
+#include "parpui.h"
+
 
 class SQLDatabase
 {
 public:
     SQLDatabase();
+    void Data_base_write(Sound* sound);
 };
 
 #endif // SQLDATABASE_H
