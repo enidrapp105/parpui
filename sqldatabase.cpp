5a5e740db2d1e545ce324f02118fc0622be66932
Enid Rapp <erapp901@gmail.com>
2026-07-05 23:50:45 -0700

    added function definitions for db read and db write

--- sqldatabase.cpp
+++ sqldatabase.cpp
@@ -1,3 +1,10 @@
 #include "sqldatabase.h"
-
 SQLDatabase::SQLDatabase() {}
+
+//QMap<QString, Sound> SQLDatabase::Database_read(){
+
+//}
+
+void SQLDatabase::Database_write(Sound* sound){
+
+}
--- sqldatabase.h
+++ sqldatabase.h
@@ -1,13 +1,14 @@
 #ifndef SQLDATABASE_H
 #define SQLDATABASE_H
+#include <QMap>
 #include "parpui.h"
-
 
 class SQLDatabase
 {
 public:
     SQLDatabase();
-    void Data_base_write(Sound* sound);
+    void Database_write(Sound* sound);
+    QMap<QString, Sound> Database_read();
 };
 
 #endif // SQLDATABASE_H
