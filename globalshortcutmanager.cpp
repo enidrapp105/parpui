dca151fad4484a3fe76840e1ffeac1ae62c20e64
Enid Rapp <erapp901@gmail.com>
2026-09-05 22:25:05 -0700

    created key array

--- globalshortcutmanager.cpp
+++ globalshortcutmanager.cpp
@@ -1,4 +1,10 @@
 #include "globalshortcutmanager.h"
+#include "backend.h"
+
+static const Qt::Key kDigitKeys[10] = {
+    Qt::Key_0, Qt::Key_1, Qt::Key_2, Qt::Key_3, Qt::Key_4,
+    Qt::Key_5, Qt::Key_6, Qt::Key_7, Qt::Key_8, Qt::Key_9,
+};
 
 GlobalShortcutManager::GlobalShortcutManager(Backend *backend, QObject *parent) {
 
